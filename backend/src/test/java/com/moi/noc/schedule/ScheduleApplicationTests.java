e66f6a3b53590f5536fab23e7aa83c81e83f4964
sopheak <nemsopheak@gmail.com>
2025-02-11 09:11:39 +0700

    fadasf

---
+++ backend/src/test/java/com/moi/noc/schedule/ScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moi.noc.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
